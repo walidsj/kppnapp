981b26752b70e7a2c90e88d13cee31b4d203e7ff
Moh. Walid Arkham Sani <mohwalidas2@gmail.com>
2021-04-23 06:56:03 +0700

    udpate info

--- resources/views/pages/admin/application_info.blade.php
+++ resources/views/pages/admin/application_info.blade.php
@@ -39,22 +39,106 @@
                         Jenderal Perbendaharaan, Kementerian Keuangan, Sistem
                         Penjadwalan dan Agenda
                     </li>
-                    <li class="list-group-item border-bottom-0">
+                    <li class="list-group-item">
                         <span class="d-block font-weight-bold">
                             <i class="fab fa-github"></i> Versi</span>
                         Lunatic 2
-                        v1.0.0
+                        v1.1.0
                     </li>
                     <li class="list-group-item border-bottom-0">
                         <span class="d-block font-weight-bold">
-                            <i class="fas fa-user"></i> Developer
+                            <i class="fas fa-user"></i> Signature
                         </span>
-                        Moh.
-                        Walid Arkham Sani<br>Tim PKL 2021 Politeknik Keuangan Negara STAN
+                        <div class="mt-3 table-responsive">
+                            <table class="table table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>No.</th>
+                                        <th>Nama</th>
+                                        <th>Program Studi</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th colspan="3">Developer</th>
+                                    </tr>
+                                    <tr>
+                                        <td>1.</td>
+                                        <td>Moh. Walid Arkham Sani</td>
+                                        <td>DIII PBB/Penilai</td>
+                                    </tr>
+                                    <tr>
+                                        <th colspan="3">Tim PKL KPPN Purwodadi 2021</th>
+                                    </tr>
+                                    <tr>
+                                        <td>1.</td>
+                                        <td>Diah Karunia Meilia Putri</td>
+                                        <td>DIII
+                                            Kebendaharaan
+                                            Negara</td>
+                                    </tr>
+                                    <tr>
+                                        <td>2.</td>
+                                        <td>Rizqi Febrian Aji</td>
+                                        <td>DIII Akuntansi</td>
+                                    </tr>
+                                    <tr>
+                                        <td>3.</td>
+                                        <td>Devita Candra Damayani</td>
+                                        <td>DIII Akuntansi</td>
+                                    </tr>
+                                    <tr>
+                                        <td>4.</td>
+                                        <td>Diah Suci Romdhonah</td>
+                                        <td>DIII Akuntansi</td>
+                                    </tr>
+                                    <tr>
+                                        <td>5.</td>
+                                        <td>Milhatul Izzah</td>
+                                        <td>DIII Akuntansi</td>
+                                    </tr>
+                                    <tr>
+                                        <td>6.</td>
+                                        <td>Shalsabilla Maulidsya
+                                            Purnomo</td>
+                                        <td>DIII
+                                            Kebendaharaan
+                                            Negara</td>
+                                    </tr>
+                                    <tr>
+                                        <td>7.</td>
+                                        <td>Satrio Bayu Adjie
+                                            Nugroho</td>
+                                        <td>DIII Akuntansi</td>
+                                    </tr>
+                                    <tr>
+                                        <td>8.</td>
+                                        <td>Arum Diah Puspitasari</td>
+                                        <td>DIII
+                                            Kebendaharaan
+                                            Negara</td>
+                                    </tr>
+                                    <tr>
+                                        <td>9.</td>
+                                        <td>Defria Fajri</td>
+                                        <td>DIII
+                                            Kebendaharaan
+                                            Negara</td>
+                                    </tr>
+                                    <tr>
+                                        <td>10.</td>
+                                        <td>Rafika Nur Sofiani</td>
+                                        <td>DIII
+                                            Kebendaharaan
+                                            Negara</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
                     </li>
                 </ul>
-                <a href="https://wa.me/6285157626557" class="btn btn-primary mt-3">
-                    <i class="fab fa-whatsapp"></i> Kontak
+                <a href="https://wa.me/6285157626557" class="btn btn-sm btn-primary mt-3">
+                    <i class="fab fa-whatsapp"></i> Kontak (Walid)
                 </a>
             </div>
         </div>
